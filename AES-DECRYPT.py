8e0b0beeba523dbc965b13f0a0f15c06a1d375de
Isthofa <75401288+isthofa-source@users.noreply.github.com>
2023-05-16 18:59:18 +0700

    Create AES-DECRYPT.py

---
+++ AES-DECRYPT.py
@@ -0,0 +1,18 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+import base64
+
+def decrypt_aes_cbc(data, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_data = cipher.decrypt(data)
+    unpadded_data = unpad(decrypted_data, AES.block_size)
+    return unpadded_data.decode('utf-8')
+
+encrypted_data = "cEIZL9HlDGNhLVrygqnXFg=="  # Data terenkripsi dalam bentuk Base64
+key = b'fastxcimbxactivo'  # Kunci enkripsi AES 128-bit (16 byte)
+iv = b'fastxcimbxactivo'  # Vektor inisialisasi (IV) 16-byte
+
+encrypted_data = base64.b64decode(encrypted_data)
+
+decrypted_data = decrypt_aes_cbc(encrypted_data, key, iv)
+print("Data Terdekripsi:", decrypted_data)
