faf6e1fdcc26ddbcef1442b0df02a8c347ac7492
Isthofa <75401288+isthofa-source@users.noreply.github.com>
2023-06-13 14:21:25 +0700

    Update and rename 1.1 AES-ENCRYPTOR-TOOLS.py to AES-ENCRYPTOR-TOOLS.py

--- AES-ENCRYPTOR-TOOLS.py
+++ AES-ENCRYPTOR-TOOLS.py
@@ -8,9 +8,9 @@ def encrypt_aes_cbc(data, key, iv):
     encrypted_data = cipher.encrypt(padded_data)
     return encrypted_data
 
-data = "Ab12!@an0nym0us"  # Data yang akan dienkripsi
-key = b'8080808080808080'  # Kunci enkripsi AES 128-bit (16 byte)
-iv = b'8080808080808080'  # Vektor inisialisasi (IV) 16-byte
+data = "YOUR-DATA"  # Data yang akan dienkripsi
+key = b'YOUR-KEY'  # Kunci enkripsi AES 128-bit (16 byte)
+iv = b'YOUR-IV'  # Vektor inisialisasi (IV) 16-byte
 
 encrypted_data = encrypt_aes_cbc(data, key, iv)
 encrypted_data_base64 = base64.b64encode(encrypted_data).decode('utf-8')
